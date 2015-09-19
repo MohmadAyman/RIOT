3aadd602acb48c74e0b4ddfd55400ed002a894a7
MohmadAyman <no.future2e1@gmail.com>
2015-09-19 22:01:28 +0200

    fixed some errors

---
+++ examples/test/main.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <periph/gpio.h>
+#include <periph_conf.h>
+
+int main(void)
+{
+    puts("Hello World!");
+    gpio_init(1,GPIO_DIR_OUT, 0);
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    gpio_init_af(3, GPIO_AF0);
+    return 0;
+}
